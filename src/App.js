edc53ebf632131559605e3e8b84c9e3879d04d9f
aiko-psi <blackaiko@gmail.com>
2018-10-23 22:43:17 +0200

    Used tutorial to test connection to server app,  added material design and axios https://hackernoon.com/tutorial-how-to-make-http-requests-in-react-part-1-f7afa3cd0cc8

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'typeface-roboto';
+import Button from '@material-ui/core/Button';
+import axios from 'axios'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    
+
+    constructor () {
+        super();
+        this.state = {taskname : ""};
+
+        this.handleClick = this.handleClick.bind(this)
+    }
+
+    handleClick(){
+        axios.get('http://localhost:8080/api/tasks/1')
+            .then(response => {
+                console.log(response);
+                this.setState({taskname: response.data.name})
+            })
+    }
+
+
+
+    render() {
+        return (
+         <div className='button__container'>
+             <Button variant="contained" color="primary" className='button' onClick={this.handleClick}>
+                 Get Taskname
+             </Button>
+             <p>{this.state.taskname}</p>
+        </div>
     );
   }
 }
